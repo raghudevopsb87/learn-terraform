0cb3cb9644b0ac6d77fbd5a5793b16f0d6070905
Raghu K <raghuk.vit@gmail.com>
2025-12-04 07:58:22 +0530

    Roboshop project

---
+++ datasources/main.tf
@@ -0,0 +1,14 @@
+data "aws_ami" "example" {
+  filter {
+    name   = "name"
+    values = ["RHEL-9-DevOps-Practice"]
+  }
+  owners = ["973714476881"]
+}
+
+
+
+output "ami" {
+  value = data.aws_ami.example
+}
+
