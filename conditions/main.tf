8e9c1a0637613c79e6caa4abe8744b84ceefbdb9
Raghu K <raghuk.vit@gmail.com>
2025-12-08 08:31:35 +0530

    Roboshop project

--- conditions/main.tf
+++ conditions/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   count         = var.create_ec2 ? 1 : 0
   ami           = "ami-09c813fb71547fc4f"
-  instance_type = var.env == "prod" ? "t3.mall" : "t3.micro"
+  instance_type = var.env == "prod" ? "t3.small" : "t3.micro"
 
   tags = {
     Name = "test"
