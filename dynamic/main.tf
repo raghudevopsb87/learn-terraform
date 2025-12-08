a028065e3f072ea195eacca74c3730c500aa2029
Raghu K <raghuk.vit@gmail.com>
2025-12-08 07:59:13 +0530

    Roboshop project

--- dynamic/main.tf
+++ dynamic/main.tf
@@ -2,6 +2,13 @@ resource "aws_security_group" "example" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic and all outbound traffic"
 
+  ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port       = 0
     to_port         = 0
