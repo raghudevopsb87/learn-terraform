f46d21a535bdb4749c5fac72a7a846a686531f8a
Raghu K <raghuk.vit@gmail.com>
2025-12-08 08:07:39 +0530

    Roboshop project

--- dynamic/main.tf
+++ dynamic/main.tf
@@ -32,3 +32,32 @@ resource "aws_security_group" "example" {
   }
 }
 
+variable "ports" {
+  default = {
+    ssh = 22
+    web = 8080
+    mon = 9090
+  }
+}
+
+resource "aws_security_group" "example1" {
+  name        = "allow_tls1"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  dynamic "ingress" {
+    for_each = var.ports
+    content {
+      from_port       = ingress.value
+      to_port         = ingress.value
+      protocol        = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
