344e15f5217f23d21d6e176a77a65d197c35388d
Raghu K <raghuk.vit@gmail.com>
2025-12-09 07:44:55 +0530

    Roboshop project

--- datasources/main.tf
+++ datasources/main.tf
@@ -29,8 +29,8 @@ output "sg" {
 }
 
 resource "aws_instance" "sample" {
-  ami = data.aws_ami.example.image_id
-  instance_type = "t3.small"
+  ami                    = data.aws_ami.example.image_id
+  instance_type          = "t3.small"
   vpc_security_group_ids = [data.aws_security_group.selected.id]
 }
 
